4822e0e782cd429ba14815c92bd91a5e0158c72f
Felix Arnold <felix.julian.arnold@gmail.com>
2021-12-12 01:04:31 +0100

    a

--- 09/low_point.py
+++ 09/low_point.py
@@ -1,24 +1,30 @@
 with open('input.txt', 'r') as input:
     data = [list(row) for row in input.read().split('\n')]
 
-print(data)
+risk_level = 0
 
-test = list()
-slope, prev_slope = [0]*2, [0]*2
+slope_y = [[1]*len(data[1]), [1]*len(data[1])]
 
 for i_row, row in enumerate(data):
+    slope_x = [1]*2
     for i_v, v in enumerate(row):
         # check next row
-        if i_row + 1 != len(data):
-            slope[0] = 1 if v <= data[i_row+1][i_v] else -1
+        if i_row < len(data) - 1:
+            slope_y[1][i_v] = int(v) - int(data[i_row+1][i_v])
         else:
-            slope[0] = 1
+            slope_y[1][i_v] = -1
 
         # check next val
-        if i_v + 1 != len(row):
-            slope[1] = 1 if v <= row[i_v+1] else -1
+        if i_v < len(row) - 1:
+            slope_x[1] = int(v) - int(row[i_v+1])
         else:
-            slope[1] = 1
+            slope_x[1] = -1
 
-        print(prev_slope, slope, v)
-        prev_slope = slope
+        if slope_x[0] > 0 and slope_x[1] < 0:
+            if slope_y[0][i_v] > 0 and slope_y[1][i_v] < 0:
+                risk_level += 1 + int(v)
+
+        slope_x.insert(0, slope_x.pop(1))
+    slope_y.insert(0, slope_y.pop(1))
+
+print(risk_level)
